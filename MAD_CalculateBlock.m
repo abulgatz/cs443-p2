64adaed9503d1db042fc237bc19cbb45e7a06bd2
joshuamiller52 <josde011@gmail.com>
2016-04-16 12:07:03 -0500

    rough first pass

--- MAD_CalculateBlock.m
+++ MAD_CalculateBlock.m
@@ -1,7 +1,12 @@
 %Assumes that Each Block is size N X N
-function MAD_Value = MAD_CalculateBlock(TargetFrameBlock,referenceFrameBlock,N,x,y,i,j)
+function MAD_Value = MAD_CalculateBlock(TargetFrame,referenceFrame,N,i,j,x,y)
+difference = 0;
+[row,col,l] = size(referenceFrame);
     for k = 0:1:N-1
         for l = 0:1:N-1
+           if((x+i+k) <= 0 || (x+i+k) > row || (y+j+l) <= 0 || (y+j+l) > col)
+               continue;
+           end
            difference = difference + abs(TargetFrame(x+k,y+l) - referenceFrame(x+i+k,y+j+l));
         end
     end
--- PsuedoCodeVersion.m
+++ PsuedoCodeVersion.m
@@ -1,4 +1,22 @@
-function a = PsuedoCodeVersion
-    min_MAD = 100000000000000;
-    for i = -p : p
+function motionVectors = PsuedoCodeVersion(targetFrame,referenceFrame,N,p)
+ [rows, cols,q] = size(targetFrame);
+ macro_block_number = 1;    
+ for x = 1 : N : rows-N+1
+    for y = 1 : N : cols-N+1
+        min_MAD = 100000000000000;
+        for i = -p:p
+           for j = -p:p
+                cur_MAD = MAD_CalculateBlock(targetFrame,referenceFrame,N,i,j,x,y);
+                if(cur_MAD < min_MAD)
+                   min_MAD = cur_MAD;
+                   u = i;
+                   v = j;
+                end
+            end
+        end
+        motionVectors(1,macro_block_number) = u;
+        motionVectors(2,macro_block_number) = v;
+        macro_block_number = macro_block_number + 1;
+    end
+ end
 end
