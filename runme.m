26f92cfa9fa881842e456ad9484156733dd01324
joshuamiller52 <josde011@gmail.com>
2016-04-20 17:32:12 -0500

    fixed the runme to be able to take any images and any size N p

--- runme.m
+++ runme.m
@@ -1,47 +1,46 @@
-rf = imread('sample1_akiyo_frame1.bmp');
-tf = imread('sample1_akiyo_frame2.bmp');
+function runme(targetFrameName,referenceFrameName,N,p)
+    rf = imread(referenceFrameName);
+    tf = imread(targetFrameName);
 
-N = 16;
-p = 7;
+    rf = rgb2gray(rf);
+    tf = rgb2gray(tf);
 
-rf = rgb2gray(rf);
-tf = rgb2gray(tf);
+    [motionVectors, mcFrame] = blockMatchingSequential(tf, rf, N, p);
+    FD = computeFrameDifference(tf, rf);
+    DFD = computeFrameDifference(tf, mcFrame);
 
-[motionVectors, mcFrame] = blockMatchingSequential(tf, rf, N, p);
-FD = computeFrameDifference(tf, rf);
-DFD = computeFrameDifference(tf, mcFrame);
+    imgLines = drawMotionVectors(mcFrame,motionVectors, N);
 
-imgLines = drawMotionVectors(mcFrame,motionVectors, N);
+    subplot(2,3,1);
+    imshow(rf);
+    title('Reference');
 
-subplot(2,3,1);
-imshow(rf);
-title('Reference');
+    subplot(2,3,2);
+    imshow(tf);
+    title('Target');
 
-subplot(2,3,2);
-imshow(tf);
-title('Target');
+    subplot(2,3,3);
+    imshow(mcFrame);
+    title('Motion Compensated');
 
-subplot(2,3,3);
-imshow(mcFrame);
-title('Motion Compensated');
+    subplot(2,3,4);
+    imshow(imgLines);
+    title('Motion Vectors');
 
-subplot(2,3,4);
-imshow(imgLines);
-title('Motion Vectors');
+    subplot(2,3,5);
+    imshow(FD);
+    title('FD');
 
-subplot(2,3,5);
-imshow(FD);
-title('FD');
+    subplot(2,3,6);
+    imshow(DFD);
+    title('DFD');
 
-subplot(2,3,6);
-imshow(DFD);
-title('DFD');
+    mse = computeMSE(tf, mcFrame);
+    psnr = computePSNR(mse);
 
-mse = computeMSE(tf, mcFrame);
-psnr = computePSNR(mse);
+    disp('mse: ');
+    disp(mse);
 
-disp('mse: ');
-disp(mse);
-
-disp('psnr: ');
-disp(psnr);
+    disp('psnr: ');
+    disp(psnr);
+end
